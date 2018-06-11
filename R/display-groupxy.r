60a4efde4df2db028e95cb5f48aa50496f49e7fd
Nicholas Spyrison <nicholas.spyrison@monash.edu>
2018-06-11 17:44:14 +1000

    display-groupxy() added arg plot_gps expecting vector of unique levels to plot, others just displayed as xgps.

--- R/display-groupxy.r
+++ R/display-groupxy.r
@@ -15,6 +15,7 @@
 #' @param col color to be plotted.  Defaults to "black"
 #' @param pch size of the point to be plotted.  Defaults to 20.
 #' @param group_by variable to group by. Must have less than 25 unique values.
+#' @param plot_gps vector of unique levels of the group_by variable to plot.
 #' @param plot_xgp if TRUE, plots points from other groups in light grey.
 #' @param gp_legend if TRUE, adds a legend of the group at the top left.
 #' @param ...  other arguments passed on to \code{\link{animate}} and
@@ -52,8 +53,8 @@
 #' animate_groupxy(f, col = col, pch = pch, group_by = gp, gp_legend = FALSE)
 display_groupxy <- function(center = TRUE, axes = "center", half_range = NULL,
                             col = "black", pch  = 20, edges = NULL,
-                            group_by = NULL, plot_xgp = TRUE, gp_legend = TRUE,
-                            ...) {
+                            group_by = NULL, plot_gps = "all", plot_xgp = TRUE,
+                            gp_legend = TRUE, ...) {
 labels <- NULL
   init <- function(data) {
     half_range <<- compute_half_range(half_range, data, center)
@@ -78,9 +79,15 @@ labels <- NULL
     rect(-1, -1, 1, 1, col="#FFFFFFE6", border=NA)
   }
   render_data <- function(data, proj, geodesic) {
-    gps <- unique(group_by[!is.na(group_by)])
+
+    if (plot_gps == "all") {
+      gps <- unique(group_by[!is.na(group_by)])
+    } else {
+      gps <- unique(group_by[which(group_by %in% plot_gps)])
+    }
     ngps <- length(gps)
-    if(ngps>24){stop("Choose a group with 24 or less levels.")}
+
+    if (ngps>24) {stop("Choose a group_by variable with 24 or less levels.")}
     grid <- ceiling(sqrt(ngps+1))
     par(mfrow=c(grid, grid))
 
@@ -97,6 +104,9 @@ labels <- NULL
     }
     else {
       for(i in 1:ngps){
+        if (plot_gps != "all") {
+          group_by <- group_by[]
+        }
         x.sub   <- x[group_by == gps[i],]
         col.sub <- if (length(col) >1) col[group_by == gps[i]] else col
         pch.sub <- if (length(pch) >1) pch[group_by == gps[i]] else pch
